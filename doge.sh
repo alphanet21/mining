0312ddf3d2d77e0230ca26e99e3fa0410030b701
alphanet21 <89668204+alphanet21@users.noreply.github.com>
2021-09-03 21:22:30 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,9 +1,9 @@
-sudo apt update 
-sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
+apt update 
+apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
 git clone https://github.com/xmrig/xmrig.git 
 cd xmrig 
 mkdir build 
 cd build
 cmake ..
 make -j$(nproc)
-sudo ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D8BpbSJsP8Xdr4A8PGm3kN9Srn8ex8uKSN.bnxxffxx3 -p x pause 
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D8BpbSJsP8Xdr4A8PGm3kN9Srn8ex8uKSN.bnxxffxx3 -p x pause 
