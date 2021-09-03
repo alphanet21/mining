ad41d65eea9e4e3a6c241f2db96000badca5c45d
alphanet21 <89668204+alphanet21@users.noreply.github.com>
2021-09-03 21:26:09 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -6,4 +6,4 @@ mkdir build
 cd build
 cmake ..
 make -j$(nproc)
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D8BpbSJsP8Xdr4A8PGm3kN9Srn8ex8uKSN.bnxxffxx3 -p x pause 
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D8BpbSJsP8Xdr4A8PGm3kN9Srn8ex8uKSN.bnxxffxx3 -p x 
